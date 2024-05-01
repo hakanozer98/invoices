ee369697f822d25c2b283cd6064d6a8872bebe9a
hakan <hakanozer98@gmail.com>
2024-05-01 12:34:56 +0300

    Initial commit
    Generated by create-expo-app 3.1.1.

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,24 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="invoices"
+        options={{
+          title: 'I"nvoices',
+          tabBarIcon: ({ color }) => <FontAwesome6 name="file-invoice" size={24} color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
